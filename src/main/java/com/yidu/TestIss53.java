6ed9a207de5b101db9e72539e5795cb659c06f78
lzw <1071203110@qq.com>
2019-07-10 15:00:42 +0800

    iss53继续开发添加了11111，提交

--- src/main/java/com/yidu/TestIss53.java
+++ src/main/java/com/yidu/TestIss53.java
@@ -4,6 +4,9 @@ public class TestIss53 {
 
 	public static void main(String[] args) {
 		System.out.println("TestIss53分枝下开发一些代码");
+		
+		//***********修复bug后继续开发************************************
+		System.out.println("11111111111111111111");
 	}
 
 }
